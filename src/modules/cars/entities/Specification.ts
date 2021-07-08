9647d5f432e883d4b276931b043204a17e77f599
Felipe Veloso <felipe.dearaujo@snhu.edu>
2021-07-08 11:37:49 -0300

    Fixed the migrations bug

--- src/modules/cars/entities/Specification.ts
+++ src/modules/cars/entities/Specification.ts
@@ -1,11 +1,21 @@
 // Como os atributos desta classe são os mesmos que os da Category.ts, poderia ser criado uma classe mais abstrata para conter todos eles e então esta classe e a Category herdariam dela. Como não temos certeza das mudanças com relação a esta classe e a outra, foi optado por não reutilizar o código. 
 
 import { v4 as uuidv4 } from 'uuid';
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm"
 
+@Entity("specifications")
 class Specification {
+
+    @PrimaryColumn()
     id?: string;
+
+    @Column()
     name: string;
+
+    @Column()
     description: string;
+
+    @CreateDateColumn()
     created_at: Date;
 
     constructor() {
--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,27 +1,27 @@
+import { getRepository, Repository } from "typeorm";
 import { Specification } from "../../entities/Specification";
 import { ISpecificationsRepository, ICreateSpecificationDTO } from "../ISpecificationsRepository";
 
 class SpecificationsRepository implements ISpecificationsRepository {
-    private specifications: Specification[];
+    private repository: Repository<Specification>;
 
     constructor() {
-        this.specifications = [];
+        this.repository = getRepository(Specification);
     }
     
-    create({ description, name }: ICreateSpecificationDTO): void {
-        const specification = new Specification();
-        
-        Object.assign(specification, {
-            description, 
-            name, 
-            created_at: new Date(),
+    async create({ description, name }: ICreateSpecificationDTO): Promise<void> {
+        const specification = this.repository.create({
+            description,
+            name,
         });
-        
-        this.specifications.push(specification);
+
+        await this.repository.save(specification);
     }
     
-    findByName(name: string): Specification {
-        const specification = this.specifications.find(spec => spec.name === name); // Como a função não está dentro de chaves, então não é preciso o return, pois ele feito automaticamente.
+    async findByName(name: string): Promise<Specification> {
+        const specification = this.repository.findOne({
+            name,
+        }) //this.specifications.find(spec => spec.name === name); // Como a função não está dentro de chaves, então não é preciso o return, pois ele feito automaticamente.
         return specification;
     }
 }
