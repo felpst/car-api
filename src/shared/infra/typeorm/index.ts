ce364be33d24b896bb2a05a0f49a0c105dbc675d
Felipe Veloso <felipe.dearaujo@snhu.edu>
2021-10-05 10:06:22 -0300

    Creating the test database

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -5,7 +5,12 @@ export default async (host = "database_felps"): Promise<Connection> => {
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host, //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+      host: process.env.NODE_ENV === "test"
+      ? "localhost"
+      : host, //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+      database: process.env.NODE_ENV === "test" // Here I am checking the NODE_ENV variable, and if it is equal test, I am running the test database instead of the production one.
+        ? "rentx_test"
+        : defaultOptions.database,
     })
   );
 }
