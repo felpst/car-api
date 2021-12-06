7376ad3d054ade9e47086bbca80bf38c16ba2ca4
Felipe Veloso <felipe.dearaujo@snhu.edu>
2021-12-06 10:02:14 -0300

    List rental by user concluded

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -4,13 +4,16 @@ import { CreateRentalController } from '@modules/rentals/useCase/createRental/Cr
 import { DevolutionRentalController } from '@modules/rentals/useCase/devolutionRental/DevolutionRentalController';
 
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+import { ListRentalsByUserController } from '@modules/rentals/useCase/listRentalsByUser/ListRentalsByUserController';
 
 const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUSerController = new ListRentalsByUserController();
 
 rentalRoutes.post("/", ensureAuthenticated, createRentalController.handle);
 rentalRoutes.post("/devolution/:id", ensureAuthenticated, devolutionRentalController.handle);
+rentalRoutes.get("/user", ensureAuthenticated, listRentalsByUSerController.handle);
 
 export { rentalRoutes }
