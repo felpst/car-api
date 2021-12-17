240d579cd9529611fd8eac6e911d8e50df90bbf0
Felipe Veloso <felipe.dearaujo@snhu.edu>
2021-12-17 10:03:07 -0300

    Refresh token controller fully implemented

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -2,7 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
 import { AppError } from "@shared/errors/AppError";
-import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
+import auth from "@config/auth";
 
 interface IPayload {
     sub: string;
@@ -14,6 +15,7 @@ export async function ensureAuthenticated(
     next: NextFunction
 ) {
     const authHeader = request.headers.authorization;
+    const usersTokensRepository = new UsersTokensRepository();
 
     if(!authHeader) {
         throw new AppError("Token missing", 401);
@@ -22,9 +24,15 @@ export async function ensureAuthenticated(
     const [, token] = authHeader.split(" "); // Since i don't need the first item of the authHeader, I will get the second by using [, secondItem]
 
     try {
-        const { sub: user_id } = verify(token, "2387913c84215ec7dd2e58c3953e9608") as IPayload; // The second parameter is the secret key that I have created for the AuthenticateUserUseCase. Using as IPayload is forcing the verify to return a sub.
-        const userRepository = new UsersRepository();
-        const user = await userRepository.findById(user_id);
+        const { sub: user_id } = verify(
+            token, 
+            auth.secret_refresh_token,
+        ) as IPayload; // The second parameter is the secret key that I have created for the AuthenticateUserUseCase. Using as IPayload is forcing the verify to return a sub.
+        
+        const user = await usersTokensRepository.findByUserIdAndRefreshToken(
+            user_id, 
+            token,
+        );
 
         if(!user) {
             throw new AppError("User does not exist!", 401);
