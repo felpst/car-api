1c8e51987b3ff497e64cacd469b137decdc42af5
Felipe Veloso <felipe.dearaujo@snhu.edu>
2022-01-31 09:36:55 -0300

    Reset password use case implemented

--- src/modules/accounts/repositories/IUsersTokensRepository.ts
+++ src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -14,6 +14,8 @@ interface IUsersTokensRepository {
     findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<UserTokens>; 
 
     deleteById(id: string): Promise<void>;
+
+    findByRefreshToken(refresh_token: string): Promise<UserTokens>;
 }
 
 export { IUsersTokensRepository }
--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { SendForgottenPasswordEmailController } from "@modules/accounts/useCases/sendForgottenPasswordEmail/SendForgottenPasswordEmailController";
+import { ResetPasswordUserController } from "@modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController";
 
 const passwordRoutes = Router();
 
 const sendForgottenPasswordEmailController = new SendForgottenPasswordEmailController();
+const resetPasswordController = new ResetPasswordUserController();
 
 passwordRoutes.post("/forgot", sendForgottenPasswordEmailController.handle);
+passwordRoutes.post("/reset", resetPasswordController.handle);
 
 export { passwordRoutes }
